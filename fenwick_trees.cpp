e90852a5640f602e89d3d1c1c583daeeda740ba1
Aarush_009 <aarushkumarchoubey@gmail.com>
2023-02-15 16:51:46 +0530

    8th commit

--- fenwick_trees.cpp
+++ fenwick_trees.cpp
@@ -1,7 +1,72 @@
 // Fenwick tree implementation
 
+// #include <bits/stdc++.h>
+// #define int long long int
+// using namespace std;
+
+// struct fenwick
+// {
+
+//     vector<int> fenWck;
+//     int n;
+
+//     void init(int n)
+//     {
+//         this->n = n + 1; // For 1 based indexing
+//         fenWck.resize(this->n, 0);
+//     }
+
+//     void add(int x, int y)
+//     {
+//         x++; // 1 based index
+//         while (x < n)
+//         {
+//             fenWck[x] += y;
+//             x += (x & (-x)); // last set bit
+//         }
+//     }
+
+//     int sum(int x)
+//     {
+//         x++; // 1 based index
+//         int ans = 0;
+//         while (x)
+//         {
+//             ans += fenWck[x];
+//             x -= (x & (-x)); // last set bit
+//         }
+
+//         return ans;
+//     }
+
+//     int sum(int l, int r)
+//     { // sum from l to r
+//         return sum(r) - sum(l - 1);
+//     }
+// };
+
+// int32_t main()
+// {
+//     vector<int> v = {1, 2, 3, 4, 5, 6, 7};
+//     fenwick tree;
+//     tree.init(v.size());
+
+//     for (int i = 0; i < v.size(); i++)
+//     {
+//         tree.add(i, v[i]);
+//     }
+
+//     cout << tree.sum(3, 5) << "\n";
+
+//     tree.add(4, -3);
+
+//     cout << tree.sum(3, 5) << "\n";
+
+//     return 0;
+// }
+
+// Inversion Count using Fenwick Trees
 #include <bits/stdc++.h>
-#define int long long int
 using namespace std;
 
 struct fenwick
@@ -9,6 +74,11 @@ struct fenwick
 
     vector<int> fenWck;
     int n;
+    fenwick() {}
+    fenwick(int n)
+    {
+        init(n);
+    }
 
     void init(int n)
     {
@@ -45,22 +115,37 @@ struct fenwick
     }
 };
 
-int32_t main()
+int main()
 {
-    vector<int> v = {1, 2, 3, 4, 5, 6, 7};
-    fenwick tree;
-    tree.init(v.size());
 
-    for (int i = 0; i < v.size(); i++)
+    int num;
+    cin >> num;
+
+    pair<int, int> akt[num];
+    for (int i = 0; i < num; i++)
     {
-        tree.add(i, v[i]);
+        cin >> akt[i].first;
+        akt[i].second = i;
     }
 
-    cout << tree.sum(3, 5) << "\n";
+    sort(akt, akt + num);
 
-    tree.add(4, -3);
+    fenwick tree(num);
 
-    cout << tree.sum(3, 5) << "\n";
+    int inv_cnt = 0, inv[num];
+
+    for (int i = 0; i < num; i++)
+    {
+        int ele = akt[i].first;
+        int ind = akt[i].second;
+
+        // we have to add in right of the index so take sum from ind+1 to n-1(i.e last element)
+        inv[ind] = tree.sum(ind + 1, num - 1);
+        inv_cnt += inv[ind];
+        tree.add(ind, 1); // update fwick tree array by 1;
+    }
+
+    cout << inv_cnt << "\n";
 
     return 0;
 }
