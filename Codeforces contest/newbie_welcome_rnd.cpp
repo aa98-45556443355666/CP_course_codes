ec9325b28c078f03134110e638b5e23b52ec9eb2
Aarush_009 <aarushkumarchoubey@gmail.com>
2023-03-12 22:02:12 +0530

    25th commit

---
+++ Codeforces contest/newbie_welcome_rnd.cpp
@@ -0,0 +1,36 @@
+// A. Lame King
+// #include <bits/stdc++.h>
+// #define int long long int
+// #define gap " "
+// using namespace std;
+
+// int32_t main()
+// {
+
+//     ios_base::sync_with_stdio(NULL);
+//     cin.tie(0);
+//     cout.tie(0);
+
+//     int testcases;
+//     cin >> testcases;
+
+//     while (testcases--)
+//     {
+
+//         int f, s;
+//         cin >> f >> s;
+
+//         f = abs(f);
+//         s = abs(s);
+
+//         if (f / s == 1)
+//         {
+//             cout << f + s << "\n";
+//         }
+//         else
+//         {
+//             cout << 2 * max(f, s) - 1 << "\n";
+//         }
+//     }
+//     return 0;
+// }
